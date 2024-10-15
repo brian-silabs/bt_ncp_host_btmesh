b3be3d7189668ec233593aece2d35f65769bd2e5
Brian <brian.rodrigues@silabs.com>
2024-10-15 17:23:14 +0200

    Initial

---
+++ config/pin_config.h
@@ -0,0 +1,171 @@
+#ifndef PIN_CONFIG_H
+#define PIN_CONFIG_H
+
+// $[ACMP0]
+// [ACMP0]$
+
+// $[ACMP1]
+// [ACMP1]$
+
+// $[ADC0]
+// [ADC0]$
+
+// $[CMU]
+// [CMU]$
+
+// $[CSEN]
+// [CSEN]$
+
+// $[DBG]
+// DBG SWV on PF2
+#ifndef DBG_SWV_PORT                            
+#define DBG_SWV_PORT                             gpioPortF
+#endif
+#ifndef DBG_SWV_PIN                             
+#define DBG_SWV_PIN                              2
+#endif
+#ifndef DBG_SWV_LOC                             
+#define DBG_SWV_LOC                              0
+#endif
+
+// [DBG]$
+
+// $[ETM]
+// [ETM]$
+
+// $[PTI]
+// [PTI]$
+
+// $[GPIO]
+// [GPIO]$
+
+// $[I2C0]
+// [I2C0]$
+
+// $[I2C1]
+// [I2C1]$
+
+// $[IDAC0]
+// [IDAC0]$
+
+// $[LESENSE]
+// [LESENSE]$
+
+// $[LETIMER0]
+// [LETIMER0]$
+
+// $[LEUART0]
+// [LEUART0]$
+
+// $[LFXO]
+// [LFXO]$
+
+// $[MODEM]
+// [MODEM]$
+
+// $[PCNT0]
+// [PCNT0]$
+
+// $[PCNT1]
+// [PCNT1]$
+
+// $[PCNT2]
+// [PCNT2]$
+
+// $[PRS.CH0]
+// [PRS.CH0]$
+
+// $[PRS.CH1]
+// [PRS.CH1]$
+
+// $[PRS.CH2]
+// [PRS.CH2]$
+
+// $[PRS.CH3]
+// [PRS.CH3]$
+
+// $[PRS.CH4]
+// [PRS.CH4]$
+
+// $[PRS.CH5]
+// [PRS.CH5]$
+
+// $[PRS.CH6]
+// [PRS.CH6]$
+
+// $[PRS.CH7]
+// [PRS.CH7]$
+
+// $[PRS.CH8]
+// [PRS.CH8]$
+
+// $[PRS.CH9]
+// [PRS.CH9]$
+
+// $[PRS.CH10]
+// [PRS.CH10]$
+
+// $[PRS.CH11]
+// [PRS.CH11]$
+
+// $[TIMER0]
+// [TIMER0]$
+
+// $[TIMER1]
+// [TIMER1]$
+
+// $[USART0]
+// [USART0]$
+
+// $[USART1]
+// [USART1]$
+
+// $[USART2]
+// USART2 RX on PB7
+#ifndef USART2_RX_PORT                          
+#define USART2_RX_PORT                           gpioPortB
+#endif
+#ifndef USART2_RX_PIN                           
+#define USART2_RX_PIN                            7
+#endif
+#ifndef USART2_RX_LOC                           
+#define USART2_RX_LOC                            9
+#endif
+
+// USART2 TX on PB6
+#ifndef USART2_TX_PORT                          
+#define USART2_TX_PORT                           gpioPortB
+#endif
+#ifndef USART2_TX_PIN                           
+#define USART2_TX_PIN                            6
+#endif
+#ifndef USART2_TX_LOC                           
+#define USART2_TX_LOC                            9
+#endif
+
+// [USART2]$
+
+// $[USART3]
+// [USART3]$
+
+// $[VDAC0]
+// [VDAC0]$
+
+// $[WTIMER0]
+// [WTIMER0]$
+
+// $[WTIMER1]
+// [WTIMER1]$
+
+// $[CUSTOM_PIN_NAME]
+#ifndef _PORT                                   
+#define _PORT                                    gpioPortA
+#endif
+#ifndef _PIN                                    
+#define _PIN                                     0
+#endif
+
+// [CUSTOM_PIN_NAME]$
+
+#endif // PIN_CONFIG_H
+
